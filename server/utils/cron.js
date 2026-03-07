145cfc5e839f7cc9d1357cde9ff540e64a19b034
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 17:04:29 +0700

    Update cron.js

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -1,40 +1,39 @@
-const cron=require("node-cron")
+const cron = require("node-cron")
 
-const Order=require("../models/Order")
-const {checkStatus}=require("./smmApi")
+const Order = require("../models/Order")
+const { checkStatus } = require("./smmApi")
+const Notification = require("../models/Notification")
 
-cron.schedule("*/5 * * * *",async()=>{
+cron.schedule("*/5 * * * *", async () => {
 
-console.log("Checking orders...")
+  console.log("Checking orders...")
 
-const orders=await Order.find({status:"processing"})
+  const orders = await Order.find({ status: "processing" })
 
-for(const o of orders){
+  for (const o of orders) {
 
-const status=await checkStatus(o.apiOrderId)
+    try {
 
-o.status=status.status
+      const status = await checkStatus(o.apiOrderId)
 
-await o.save()
+      o.status = status.status
+      await o.save()
 
-}
+      if (status.status === "completed") {
 
-})
+        await Notification.create({
+          userId: o.userId,
+          message: "Đơn " + o._id + " đã hoàn thành"
+        })
 
-const Notification=require("../models/Notification")
+      }
 
-if(status.status==="completed"){
+    } catch (err) {
 
-await Notification.create({
+      console.log("Cron error:", err)
 
-userId:o.userId,
-message:"Đơn "+o._id+" đã hoàn thành"
+    }
 
-})
-
-}
-io.emit("notification",{
-
-message:"Đơn "+o._id+" đã hoàn thành"
+  }
 
 })
