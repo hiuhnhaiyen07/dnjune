b67d3a68b66a99271124cbb2536020e252f95074
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 22:44:37 +0700

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,15 +4,29 @@ const User = require("../models/User")
 
 const router = express.Router()
 
+/* GET USER INFO */
+
 router.get("/me", auth, async (req,res)=>{
 
+try{
+
 const user = await User.findById(req.user.id)
+
+if(!user){
+return res.json({error:"User không tồn tại"})
+}
 
 res.json({
 username:user.username,
 balance:user.balance,
 email:user.email
 })
+
+}catch(err){
+
+res.status(500).json({error:"Server error"})
+
+}
 
 })
 
