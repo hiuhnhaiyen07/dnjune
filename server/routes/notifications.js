a990b3f4bd6a347c9c23815ccf7fcf2501391afe
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 16:15:04 +0700

    Create notifications.js

---
+++ server/routes/notifications.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const auth=require("../middleware/auth")
+
+const Notification=require("../models/Notification")
+
+const router=express.Router()
+
+router.get("/",auth,async(req,res)=>{
+
+const data=await Notification.find({
+userId:req.user.id
+}).sort({createdAt:-1})
+
+res.json(data)
+
+})
+
+module.exports=router
