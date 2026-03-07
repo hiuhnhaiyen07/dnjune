da338c48fc199ad2301cec7587f9b4fa4492b979
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 20:51:17 +0700

    Create services.js

---
+++ server/config/services.js
@@ -0,0 +1,53 @@
+module.exports = [
+
+{
+category: "Facebook Likes",
+services: [
+
+{
+id: 1,
+name: "Facebook Like Post",
+provider: 1234,
+rate: 0.8,
+min: 100,
+max: 10000
+},
+
+{
+id: 2,
+name: "Facebook Page Likes",
+provider: 1235,
+rate: 1.2,
+min: 50,
+max: 5000
+}
+
+]
+},
+
+{
+category: "Tiktok",
+services: [
+
+{
+id: 3,
+name: "Tiktok Followers",
+provider: 2001,
+rate: 2.5,
+min: 100,
+max: 20000
+},
+
+{
+id: 4,
+name: "Tiktok Likes",
+provider: 2002,
+rate: 0.6,
+min: 100,
+max: 50000
+}
+
+]
+}
+
+]
