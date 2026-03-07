b51f0140894f1607f1544a190e999bb1873686f3
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 16:01:11 +0700

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  balance: {
+    type: Number,
+    default: 0
+  },
+
+  role: {
+    type: String,
+    default: "user"
+  },
+
+  isBlocked: {
+    type: Boolean,
+    default: false
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model("User", UserSchema)
