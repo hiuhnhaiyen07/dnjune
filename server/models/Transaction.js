1023155b884e2d0276fce564383ee35b7e424425
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 21:18:36 +0700

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -2,13 +2,27 @@ const mongoose = require("mongoose")
 
 const TransactionSchema = new mongoose.Schema({
 
-userId:String,
+userId:{
+type:mongoose.Schema.Types.ObjectId,
+ref:"User",
+required:true
+},
 
+type:{
 type:String,
+enum:["deposit","order","refund","admin_add","admin_sub"],
+required:true
+},
 
-amount:Number,
+amount:{
+type:Number,
+required:true
+},
 
-note:String,
+note:{
+type:String,
+default:""
+},
 
 createdAt:{
 type:Date,
