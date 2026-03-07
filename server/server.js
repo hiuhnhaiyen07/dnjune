01e145fedf5d73bdee3e04cc3154edd4d8eaf3b8
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 23:00:59 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,6 +38,29 @@ app.use("/api/services", serviceRoutes)
 app.use("/api/user", userRoutes)
 
 /* =========================
+   MONGODB CONNECT
+========================= */
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+console.log("MongoDB connected")
+})
+.catch(err=>{
+console.log("Mongo error:",err)
+})
+
+/* =========================
+   SERVER START
+========================= */
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+console.log("Server running on port "+PORT)
+})app.use("/api/services", serviceRoutes)
+app.use("/api/user", userRoutes)
+
+/* =========================
    DATABASE
 ========================= */
 
