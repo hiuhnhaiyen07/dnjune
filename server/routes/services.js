98bcd199fee3528fa6f1f0937c504cb5bf1df983
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 16:03:13 +0700

    Create services.js

---
+++ server/routes/services.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const { getServices } = require("../utils/smmApi")
+
+const auth = require("../middleware/auth")
+
+const router = express.Router()
+
+router.get("/", auth, async (req, res) => {
+
+  try {
+
+    const services = await getServices()
+
+    res.json(services)
+
+  } catch (err) {
+
+    res.status(500).json({ error: "API error" })
+
+  }
+
+})
+
+module.exports = router
