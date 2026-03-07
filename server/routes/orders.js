8f065dd9a71f82c67d874f9c03dd65339547c1c5
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 17:23:19 +0700

    Update orders.js

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,80 +1,6 @@
 const express = require("express")
 
 const auth = require("../middleware/auth")
-
-const { createOrder, checkStatus } = require("../utils/smmApi")
-
-const Order = require("../models/Order")
-const User = require("../models/User")
-
-const router = express.Router()
-
-/* CREATE ORDER */
-
-router.post("/create", auth, async (req, res) => {
-
-  const { service, link, quantity, price } = req.body
-
-  const user = await User.findById(req.user.id)
-
-  if (user.balance < price) {
-    return res.json({ error: "Không đủ số dư" })
-  }
-
-  const api = await createOrder(service, link, quantity)
-
-  const order = new Order({
-
-    userId: user._id,
-    service,
-    link,
-    quantity,
-    price,
-    status: "processing",
-    apiOrderId: api.order
-
-  })
-
-  await order.save()
-
-  user.balance -= price
-  await user.save()
-
-  res.json({ message: "Đặt đơn thành công" })
-
-})
-
-/* CHECK STATUS */
-
-router.get("/status/:id", auth, async (req, res) => {
-
-  const order = await Order.findById(req.params.id)
-
-  const status = await checkStatus(order.apiOrderId)
-
-  order.status = status.status
-
-  await order.save()
-
-  res.json(status)
-
-})
-
-/* USER ORDERS */
-
-router.get("/my", auth, async (req, res) => {
-
-  const orders = await Order.find({ userId: req.user.id }).sort({ createdAt: -1 })
-
-  res.json(orders)
-
-})
-
-module.exports = router
-
-const express = require("express")
-
-const auth = require("../middleware/auth")
 const antiSpam = require("../middleware/antiSpam")
 
 const { createOrder, checkStatus } = require("../utils/smmApi")
@@ -116,8 +42,6 @@ router.post("/create", auth, antiSpam, async (req, res) => {
     user.balance -= price
     await user.save()
 
-    /* SAVE TRANSACTION */
-
     await Transaction.create({
       userId: user._id,
       type: "order",
@@ -146,6 +70,7 @@ router.get("/status/:id", auth, async (req, res) => {
     const status = await checkStatus(order.apiOrderId)
 
     order.status = status.status
+
     await order.save()
 
     res.json(status)
@@ -162,19 +87,11 @@ router.get("/status/:id", auth, async (req, res) => {
 
 router.get("/my", auth, async (req, res) => {
 
-  try {
+  const orders = await Order
+    .find({ userId: req.user.id })
+    .sort({ createdAt: -1 })
 
-    const orders = await Order
-      .find({ userId: req.user.id })
-      .sort({ createdAt: -1 })
-
-    res.json(orders)
-
-  } catch (err) {
-
-    res.status(500).json({ error: "Server error" })
-
-  }
+  res.json(orders)
 
 })
 
