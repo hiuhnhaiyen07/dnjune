88ffae3449bc47a5f36fe788d4e57bca0e619607
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 21:01:52 +0700

    Update Payment.js

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,17 +1,34 @@
 const mongoose = require("mongoose")
 
 const PaymentSchema = new mongoose.Schema({
-  userId: String,
-  amount: Number,
-  billImage: String,
-  status: {
-    type: String,
-    default: "pending"
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+
+userId:{
+type:mongoose.Schema.Types.ObjectId,
+ref:"User",
+required:true
+},
+
+amount:{
+type:Number,
+required:true
+},
+
+billImage:{
+type:String,
+required:true
+},
+
+status:{
+type:String,
+enum:["pending","approved","rejected"],
+default:"pending"
+},
+
+createdAt:{
+type:Date,
+default:Date.now
+}
+
 })
 
 module.exports = mongoose.model("Payment", PaymentSchema)
