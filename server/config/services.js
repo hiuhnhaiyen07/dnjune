0fc3fc0533cf6e9a8c128533ea61347566643e27
hiuhnhaiyen07 <hiuhnhaiyen27@gmail.com>
2026-03-07 21:36:57 +0700

    Update services.js

--- server/config/services.js
+++ server/config/services.js
@@ -1,50 +1,124 @@
 module.exports = [
 
 {
-category: "Facebook Likes",
+category: "FACEBOOK",
 services: [
 
 {
 id: 1,
-name: "Facebook Like Post",
-provider: 1234,
-rate: 0.8,
+name: "Follow Facebook [Ẩn]",
+provider: 2614,
+rate: 5000,
 min: 100,
 max: 10000
 },
 
 {
 id: 2,
-name: "Facebook Page Likes",
-provider: 1235,
-rate: 1.2,
-min: 50,
-max: 5000
-}
-
-]
+name: "Follow Facebook [Ẩn]",
+provider: 2771,
+rate: 6400,
+min: 100,
+max: 10000
 },
 
 {
-category: "Tiktok",
-services: [
+id: 3,
+name: "Follow Facebook [Tài Nguyên Page Việt + Ngoại]",
+provider: 2652,
+rate: 8500,
+min: 100,
+max: 10000
+},
 
 {
-id: 3,
-name: "Tiktok Followers",
-provider: 2001,
-rate: 2.5,
+id: 4,
+name: "Follow Facebook Việt",
+provider: 2653,
+rate: 9000,
+min: 100,
+max: 10000
+},
+
+{
+id: 5,
+name: "Cảm Xúc ❤️",
+provider: 2695,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 6,
+name: "Cảm Xúc 🥰",
+provider: 2696,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 7,
+name: "Cảm Xúc 😮",
+provider: 2697,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 8,
+name: "Cảm Xúc 😂",
+provider: 2698,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 9,
+name: "Cảm Xúc 😢",
+provider: 2699,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 10,
+name: "Cảm Xúc 😡",
+provider: 2700,
+rate: 5000,
+min: 100,
+max: 50000
+},
+
+{
+id: 11,
+name: "View Story",
+provider: 364,
+rate: 17000,
 min: 100,
 max: 20000
 },
 
 {
-id: 4,
-name: "Tiktok Likes",
-provider: 2002,
-rate: 0.6,
+id: 12,
+name: "View Story",
+provider: 365,
+rate: 10000,
 min: 100,
-max: 50000
+max: 20000
+},
+
+{
+id: 13,
+name: "Thành Viên Nhóm [VIỆT]",
+provider: 2710,
+rate: 10000,
+min: 100,
+max: 10000
 }
 
 ]
